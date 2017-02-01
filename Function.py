856efd56fe5fac8f923e23b7faf7dc8aa22a457f
IMissHarmbe <wilsonbe011800@cvh.rusd.us>
2017-02-01 10:47:28 -0800

    User prompt done

--- Function.py
+++ Function.py
@@ -3,7 +3,7 @@ the result"""
 def triple(x):
     y = x*3
     return y
-print(triple(10))    
+#print(triple(10))
 
 
 
@@ -11,3 +11,6 @@ print(triple(10))
 """Write a program that will prompt the user for an input value (n) and print
 the result of 3n by calling the function defined above.  Make sure you include
 the necessary print statements and address any issues with whitespace. """
+print ("Hello you smelly dingus, give me a number to triple")
+d = int(input())
+print(triple(d))
