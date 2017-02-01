0f67406610a48a889b66582dba29f67ce5ac4489
IMissHarmbe <wilsonbe011800@cvh.rusd.us>
2017-02-01 11:30:17 -0800

    40 to 1

--- RangeFunction.py
+++ RangeFunction.py
@@ -1,10 +1,14 @@
 """Use the range function to print the numbers from 40 to 1 (backwards)"""
+def heck(x):
+    while x > 1:
+        x = x - 1
+        print (x)
 
-
+print(heck(41))
 
 """Repeat the exercise but count by 5's"""
 
 
 
-"""Write a program that will count print all the multiples of (n) where n is
+"""Write a program that will count print first 10 multiples of (n) where n is
 taken from user input.  Include necessary print statements."""
