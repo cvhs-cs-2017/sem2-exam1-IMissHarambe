d341de50afe60f9a64a29075cf24f9d5e781ad2a
IMissHarmbe <wilsonbe011800@cvh.rusd.us>
2017-02-01 11:42:41 -0800

    succefully imported

--- Turtle.py
+++ Turtle.py
@@ -1,8 +1,28 @@
 """Create a Turtle Program that will draw a 3-dimensional cube"""
+import turtle
+weenr = turtle.Turtle
 
 
 
 
 
-"""Import and Call the DrawRectangle(Anyturtle, l, w) function from the
-file MyFile.py"""
+"""Import and Call the DrawRectangle(Anyturtle, l, w) function from the file MyFile.py"""
+
+from MyFile.py import DrawRectangle
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+input()
