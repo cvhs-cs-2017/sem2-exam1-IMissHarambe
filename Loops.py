9ff39b0c4f37d02fabc9371ef744048b78e82cb5
IMissHarmbe <wilsonbe011800@cvh.rusd.us>
2017-02-01 11:04:28 -0800

    add5 done

--- Loops.py
+++ Loops.py
@@ -1,8 +1,13 @@
 """Write a program that will add 5 and loop until it reaches a number GREATER
 than 100.  It should then spit out the result AND tell the user how many times
 it had to add 5 (if any)"""
-
-
+def add5until100():
+    x = 0
+    while x < 100:
+        x = x + 5
+        print (x)
+    print("It added five 20 times")    
+print (add5until100())
 
 
 
