e98dccef391336ac26927fc6c21a528896587b4a
IMissHarmbe <wilsonbe011800@cvh.rusd.us>
2017-02-01 11:10:47 -0800

    no vowels

--- Encrypt.py
+++ Encrypt.py
@@ -3,11 +3,32 @@
 'Computer Science Makes the World go round but it doesn't make the world round itself!'
 
 Print the save the result as the variable = NoVowels
-""""
-
-
-
-
-
-
-"""Write an encryption code that you make up and run it for the variable NoVowels"""
+"""
+def nohaksheer(x):
+    vowels = "AaEeIiOoUu"
+    novowels = ""
+    for y in x:
+        if y == 'a':
+            novowels = novowels + " "
+        elif y == "A":
+            novowels = novowels + " "
+        elif y == "E":
+            novowels = novowels + " "
+        elif y == "e":
+            novowels = novowels + " "
+        elif y == "I":
+            novowels = novowels + " "
+        elif y == "i":
+            novowels = novowels + " "
+        elif y == "O":
+            novowels = novowels + " "
+        elif y == "o":
+            novowels = novowels + " "
+        elif y == "U":
+            novowels = novowels + " "
+        elif y == "u":
+            novowels = novowels + " "
+        else:
+            novowels = novowels + y
+    return novowels
+print(nohaksheer('Computer Science Makes the World go round but it doesn\'t make the world round itself!'))
