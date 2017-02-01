265ac7011ca77a8426e7dbb01052d5df6f90f921
IMissHarmbe <wilsonbe011800@cvh.rusd.us>
2017-02-01 10:39:33 -0800

    finished drawpoly

--- MyFile.py
+++ MyFile.py
@@ -12,4 +12,10 @@ def DrawRectangle (Anyturtle, l, w):
         Anyturtle.right(90)
         Anyturtle.forward(w)
         Anyturtle.right(90)
-DrawRectangle(dingus, 100, 50)
+#DrawRectangle(dingus, 100, 50)
+def DrawPoly(Anyturtle, n):
+    for i in range (n):
+        Anyturtle.forward(50)
+        Anyturtle.right(50)
+    Anyturtle.goto(0,0)
+DrawPoly (dingus, 7)
