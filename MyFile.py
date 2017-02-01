8e4c6fe372473f4217758444dedeb9683e8d93e5
IMissHarmbe <wilsonbe011800@cvh.rusd.us>
2017-02-01 10:29:07 -0800

    drawrectangle done

--- MyFile.py
+++ MyFile.py
@@ -3,3 +3,13 @@ def AddTen(n):
     return x
 
 print (AddTen(10))
+
+import turtle
+dingus = turtle.Turtle()
+def DrawRectangle (Anyturtle, l, w):
+    for i in range (2):
+        Anyturtle.forward(l)
+        Anyturtle.right(90)
+        Anyturtle.forward(w)
+        Anyturtle.right(90)
+DrawRectangle(dingus, 100, 50)
