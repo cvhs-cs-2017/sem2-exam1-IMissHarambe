39473eba7267b08884e9def1c87ab27ed1318844
IMissHarmbe <wilsonbe011800@cvh.rusd.us>
2017-02-01 11:33:13 -0800

    first ten done

--- RangeFunction.py
+++ RangeFunction.py
@@ -4,7 +4,7 @@ def heck(x):
         x = x - 1
         print (x)
 
-print(heck(41))
+#print(heck(41))
 
 """Repeat the exercise but count by 5's"""
 def darn(x):
@@ -12,9 +12,14 @@ def darn(x):
         x = x - 5
         print (x)
 
-print(darn(40))
+#print(darn(40))
 
 
 
 """Write a program that will count print first 10 multiples of (n) where n is
 taken from user input.  Include necessary print statements."""
+def tenmultiples(y):
+    for y in range(10):
+        y = y*10
+        print(y)
+print (tenmultiples(10))
