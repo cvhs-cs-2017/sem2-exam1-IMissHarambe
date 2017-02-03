41f132ef68780c30e24de681953e8c735aa2c2bb
IMissHarmbe <wilsonbe011800@cvh.rusd.us>
2017-02-03 10:31:12 -0800

    finished the 3d cube

--- Turtle.py
+++ Turtle.py
@@ -1,14 +1,25 @@
 """Create a Turtle Program that will draw a 3-dimensional cube"""
 import turtle
-weenr = turtle.Turtle
+weenr = turtle.Turtle()
+def threeDcube():
+    for i in range(4):
+        weenr.forward(50)
+        weenr.right(90)
+    weenr.goto(25,25)
+    for i in range(4):
+        weenr.forward(50)
+        weenr.right(90)
+    weenr.forward(50)
+    weenr.goto(50,0)
+    weenr.right(90)
+    weenr.forward(50)
+    weenr.goto(75,-25)
 
-
-
-
+threeDcube()
 
 """Import and Call the DrawRectangle(Anyturtle, l, w) function from the file MyFile.py"""
 
-from MyFile.py import DrawRectangle
+#from MyFile.py import DrawRectangle
 
 
 
